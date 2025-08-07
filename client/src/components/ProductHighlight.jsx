9307321defa37db2282b3df92fd28c02ff5f02bc
First <first.ruankaeo@gmail.com>
2025-08-07 10:40:57 +0700

    feat:add answer to mission 3

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -9,13 +9,16 @@ function ProductHighlight() {
           {/* 🛠️ Todo 2: ให้ส่งข้อมูลเข้าไปใน Component ProductCard ผ่าน Prop ที่ชื่อว่า `imgSrc`
             
             ==== ตัวอย่าง ====
-
-            <ProductCard imgSrc="https://picsum.photos/id/231/200/200" />
-            
         */}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+        <ProductCard imgSrc="https://picsum.photos/id/231/200/200" 
+         productName = "The Lorem Ipsum for photos."
+         productPrice = "$99.99"/>
+        <ProductCard imgSrc="https://picsum.photos/id/232/200/200" 
+        productName = "Wireless Bluetooth Earbuds"
+        productPrice = "$79.99"/>
+        <ProductCard imgSrc="https://picsum.photos/200/200" 
+        productName = "Stylish Cool Sunglasses in The World"
+        productPrice = "$79.99"/>
         </div>
       </div>
     </section>
